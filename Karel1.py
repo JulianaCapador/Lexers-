36016fa79cbe09bc26c80c1bde7be26a76e40d40
JulianaCapador <jalexaco@gmail.com>
2018-11-19 12:35:10 -0500

    una version funcional

--- Karel1.py
+++ Karel1.py
@@ -2,8 +2,16 @@ import ply.lex as lex
 import codecs 
 
 programa = ['INICIAR_PROGRAMA','INICIA_EJECUCION','TERMINA_EJECUCION','FINALIZAR_PROGRAMA']
+
+expresion = ['APAGATE','GIRA_IZQUIERDA','AVANZA','COGE_ZUMBADOR','DEJA_ZUMBADOR','SAL_DE_INSTRUCCION','INICIO','FIN']
+
 reservadas = ['SI','NETONCES','SINO','MIENTRAS','HACER','REPETIR','VECES']
-tokens = reservadas+programa + ['LETRA','NUMBER','PLUS','TIMES','DIVIDE', 'EQUALS','SEMICOLON','CADENA','COMMENT_SAME_LINE_PASCAL','COMMENT_SAME_LINE_JAVA']
+
+fBooleana = ['FRENTE_LIBRE','FRENTE_BLOQUEADO','IZQUIERDA_LIBRE','IZQUIERDA_BLOQUEADA','DERECHA_LIBRE','DERECHA_BLOQUEADA',
+             'JUNTO_A_ZUMBADOR','NO_JUNTO_A_ZUMBADOR','ALGUN_ZUMBADOR_EN_LA_MOCHILA','NINGUN_ZUMBADOR_EN_LA_MOCHILA','ORIENTADO_AL_NORTE',
+             'ORIENTADO_AL_SUR','ORIENTADO_AL_ESTE','ORIENTADO_AL_OESTE','NO_ORIENTADO_AL_NORTE','NO_ORIENTADO_AL_SUR','NO_ORIENTADO_AL_ESTE','NO_ORIENTADO_AL_OESTE']
+             
+tokens = expresion+reservadas+programa+fBooleana + ['LETRA','NUMBER','PLUS','TIMES','DIVIDE', 'EQUALS','SEMICOLON','CADENA','COMMENT_SAME_LINE_PASCAL','COMMENT_SAME_LINE_JAVA']
 
 t_ignore = ' \t'
 t_PLUS = r'\+'
@@ -39,12 +47,8 @@ def t_SPACE(t):
     pass
 
 def t_CADENA(t):
-    r'\".*'
+    r'\".*"'
     return t
-
-#def t_INICIAR(t):
-    #r'\coge-zumbador'
-    #return t
 
 # Error handling rule
 def t_error(t):
